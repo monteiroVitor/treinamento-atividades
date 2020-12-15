104a743258c13cdd1aec825e5bb07f9abed871b2
Vitor M Mendonça <monteiromendoncavitor@gmail.com>
2020-12-15 20:10:43 -0300

    Aplicando todas as modificações no server: retirando rotas, conexão com o banco, usando express no lugar do bodyParser

--- 14-12-2020/server.js
+++ 14-12-2020/server.js
@@ -1,78 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+import express from 'express';
+import ConnectDB from './src/dbConnect.js';
+import userRoutes from './src/routes/user.js';
+import path from 'path';
 const app = express();
+let db;
 
-const ObjectId = require('mongodb').ObjectID;
-const MongoClient = require('mongodb').MongoClient;
-const { DB_URI } = require('./helpers/config');
+ConnectDB()
+  .then((database) => (db = database))
+  .then(() => app.listen(3000))
+  .catch((err) => console.log(err));
 
-MongoClient.connect(
-  DB_URI,
-  { useUnifiedTopology: true, useNewUrlParser: true },
-  (err, client) => {
-    if (err) return console.log(err);
-    db = client.db('treinamentoDb');
-    app.listen(3000, () => console.log('servidor rodando na porta 3000'));
-  }
-);
-
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.set('view egine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.render('index.ejs');
-});
+//middleware
+const dbReference = (req, res, next) => {
+  req.database = db;
+  next();
+};
 
-app.post('/show', (req, res) => {
-  db.collection('data').save(req.body, (err, result) => {
-    if (err) return console.log(err);
-
-    console.log('Salvando no Banco de Dados');
-    res.redirect('/show');
-  });
-});
-
-app.get('/show', (req, res) => {
-  db.collection('data')
-    .find()
-    .toArray((err, results) => {
-      if (err) return console.log(err);
-      res.render('show.ejs', { data: results });
-    });
-});
-
-app
-  .route('/edit/:id')
-  .get((req, res) => {
-    const { id } = req.params;
-    db.collection('data')
-      .find(ObjectId(id))
-      .toArray((err, result) => {
-        if (err) return console.log(err);
-        res.render('edit.ejs', { data: result });
-      });
-  })
-  .post((req, res) => {
-    const { id } = req.params;
-    const { name, surname } = req.body;
-    db.collection('data').updateOne(
-      { _id: ObjectId(id) },
-      {
-        $set: { name, surname },
-      },
-      (err, result) => {
-        if (err) return res.send(err);
-        res.redirect('/show');
-        console.log('Atualizando no Banco de Dados');
-      }
-    );
-  });
-
-app.route('/delete/:id').get((req, res) => {
-  const { id } = req.params;
-  db.collection('data').deleteOne({ _id: ObjectId(id) }, (err, result) => {
-    if (err) return res.send(500, err);
-    console.log('Deletado do Banco de Dados!');
-    res.redirect('/show');
-  });
-});
+//Rotas
+app.use('/user', dbReference, userRoutes);
