5f5ae5ca5280918501d1f1b436a0246e60c9bb98
Vitor M Mendonça <monteiromendoncavitor@gmail.com>
2020-12-16 14:25:46 -0300

    Integrando novas rotas

--- 14-12-2020/server.js
+++ 14-12-2020/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import ConnectDB from './src/dbConnect.js';
 import userRoutes from './src/routes/user.js';
-import path from 'path';
+import campgroundRoutes from './src/routes/campground.js';
+import indexRoutes from './src/routes/index.js';
+
 const app = express();
 let db;
 
@@ -21,4 +23,6 @@ const dbReference = (req, res, next) => {
 };
 
 //Rotas
+app.use('/', indexRoutes);
 app.use('/user', dbReference, userRoutes);
+app.use('/campground', dbReference, campgroundRoutes);
